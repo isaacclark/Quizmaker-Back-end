168333fe2df622c7c40cb5ffc6892dfd6d790fd8
clarki3 <clarki3@uni.coventry.ac.uk>
2020-07-31 20:37:22 +0100

    Added a DB in addons and replaced the db config data with the new db hosted through jawsdb

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 //use the value stored in the environment otherwise use the default one
 exports.config = {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "fojvtycq53b2f2kx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
     port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_DATABASE || "oktob",
+    user: process.env.DB_USER || "d0gb7qyizgcx4yce",
+    password: process.env.DB_PASSWORD || "ahhev177szx6o179",
+    database: process.env.DB_DATABASE || "q74d7po7a3xyem92",
     connectionLimit: 100
 }
--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const conData = {
-    host:"localhost",
-    user:"root",
-    password: "",
-    database: "oktob"
+    host:"fojvtycq53b2f2kx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+    user:"d0gb7qyizgcx4yce",
+    password: "ahhev177szx6o179",
+    database: "q74d7po7a3xyem92"
 };
 
 exports.connect = function( callback){
